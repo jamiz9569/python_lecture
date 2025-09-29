c28097fdf2ba0c08f54ac219f6ede78a4af3d35f
jamiz9569 <jamizalam1@gmail.com>
2025-09-30 03:15:14 +0530

    datatype and random no.

---
+++ intro2.py
@@ -0,0 +1,66 @@
+"""
+Text Type:	str
+Numeric Types:	int, float, complex
+Sequence Types:	list, tuple, range
+Mapping Type:	dict
+Set Types:	set, frozenset
+Boolean Type:	bool
+Binary Types:	bytes, bytearray, memoryview
+None Type:	NoneType
+
+""" 
+
+
+
+
+
+a = "Hello World" #	str	
+b = 20	#int	
+c = 20.5  #	float	
+d = 1j	#complex	
+e = ["apple", "banana", "cherry"]	#list	
+f = ("apple", "banana", "cherry")	#tuple	
+g = range(6)	#range	
+h = {"name" : "John", "age" : 36}	#dict	
+i = {"apple", "banana", "cherry"}	#set	
+j = frozenset({"apple", "banana", "cherry"})  #	frozenset	
+k = True	#bool	
+l = b"Hello"	#bytes	
+m= bytearray(5)	#bytearray	
+n = memoryview(bytes(5))   #	memoryview	
+o = None   #  	NoneType
+
+
+print (type(a))
+print (type(b)) 
+print (type(c))
+print (type(d))
+print (type(e))
+print (type(f))
+print (type(g))
+print (type(h))
+print (type(i))
+print (type(j))
+print (type(k))
+print (type(l))
+print (type(m))
+print (type(n))
+print (type(o))
+
+
+
+
+"""
+
+
+Random Number
+Python does not have a random() function to make a random number, but Python has a built-in module called random that can be used to make random numbers:
+
+Example
+Import the random module, and display a random number from 1 to 9:
+
+"""
+import random
+
+print(random.randrange(1, 10))
+
