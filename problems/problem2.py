7d91020459514c092a2bf7dce8d664edda9849a3
jamiz qamar <jamizalam1@gmail.com>
2026-01-11 17:17:36 +0530

    variable and datatype

---
+++ problems/problem2.py
@@ -0,0 +1,28 @@
+# question 1
+
+a = int(input("enter first number: "))
+b = int(input("enter second number: "))
+sum = a+b 
+print(sum)
+
+# question 2
+
+c = int(input("enter numer: "))
+rem = c% 2 
+print(rem)
+
+# question 3
+d=int(input("enter number: "))
+e= int (input("enter another number: "))
+print("d is greater than e: ", d>e)
+
+# question 4
+f = int(input("enter number: "))
+g = int(input("enter another number: "))
+avg = (f+g)/2
+print(avg)
+
+# question 5
+h = int(input("enter number: "))
+square = h**2
+print(square)
